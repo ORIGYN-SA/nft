b90ee3ef6a92b76e4a6192e7f8f70c39fa3db50c
Gautier Wojda <gautier.wojda@bity.com>
2025-06-19 12:26:26 +0200

    update ci

--- scripts/run_integrations_tests.sh
+++ scripts/run_integrations_tests.sh
@@ -2,8 +2,9 @@
 
 ulimit -n 65536
 
-./scripts/build_all.sh
+./scripts/build.sh
 
+mkdir -p integrations_tests/wasm
 cp src/core_nft/wasm/core_nft_canister.wasm.gz integrations_tests/wasm/
 
 cargo test -p integration_tests -- --test-threads=1
